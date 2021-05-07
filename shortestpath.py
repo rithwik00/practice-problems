85b631550653d65863329421b384bbadfe37a0a4
Rithwik <vedpathakrithwik@gmail.com>
2021-05-07 22:09:07 +0530

    small change

--- shortestpath.py
+++ shortestpath.py
@@ -1,7 +1,7 @@
 '''
 You are given a square grid with some cells open (.) and some blocked (X). 
 Your playing piece can move along any row or column until it reaches the edge of the grid or a blocked cell.
- Given a grid, a start and a goal, determine the minmum number of moves to get to the goal.
+Given a grid, a start and a goal, determine the minmum number of moves to get to the goal.
 
 Example.
 
